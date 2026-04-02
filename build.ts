7de1c47421bf95586bd1f33ae28741ef10e86cc4
Francis Judge <francis@fjsolutions.co.za>
2026-04-02 19:51:19 +0200

    Updated and simplified the build process

--- build.ts
+++ build.ts
@@ -2,10 +2,10 @@ import * as esbuild from "esbuild"
 
 await esbuild.build({
    entryPoints: ["src/index.ts"],
-   bundle: true,
+   bundle: false,
    outfile: "dist/index.js",
    format: "esm",
    platform: "neutral",
    target: "es2016",
-   minify: true,
+   minify: false,
 })
